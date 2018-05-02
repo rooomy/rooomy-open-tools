d96952224427d65b469e7681fb83b0b23691bcc7
Ribamar Santarosa <ribamar-santarosa@rooomy.com>
2018-05-02 10:18:13 +0000

    rubyrooomy: rubyrooomy.rb:  function define_contexts: added

--- rubyrooomy/rubyrooomy.rb
+++ rubyrooomy/rubyrooomy.rb
@@ -14,6 +14,113 @@ require "rubyment"
 # benefit from the RubyGem functions
 #  support.
 class RubyRooomy < Rubyment
+
+  # define_contexts takes as parameter
+  # an Array of Hashes having Arrays as values
+  # (which we call "basic definition of
+  # a test case") and returns an expanded
+  # Array of Hashes (which we call "an Array
+  # of test contexts"). Each Hash in the
+  # latter has the same keys of its source
+  # Hash, but instead of the Array as value, it
+  # has only one of those values. All the
+  # possible combinations will be generated.
+  #
+  # Probably it's clearer with an example:
+  # suppose in a system you want to test
+  # if a user role could create another
+  # user role.
+  #
+  # SAMPLE INPUT:
+  # [
+  #   {
+  #     "role" : [role_3, role_4],
+  #     "authenticate_as": [role_1, role_2],
+  #     "result": ["allowed"],
+  #   },
+  #   {
+  #     "role" : [role_1, role_2, role_3, role_4] - [role_3, role_4],
+  #     "authenticate_as": [role_1, role_2],
+  #     "result": ["disallowed"],
+  #   },
+  # ]
+  #
+  # SAMPLE_OUTPUT:
+  # [
+  #   {
+  #     "role" : role_3,
+  #     "authenticate_as": role_1,
+  #     "result": "allowed",
+  #   },
+  #   {
+  #     "role" : role_4,
+  #     "authenticate_as": role_1,
+  #     "result": "allowed",
+  #   },
+  #   {
+  #     "role" : role_3,
+  #     "authenticate_as": role_2,
+  #     "result": "allowed",
+  #   },
+  #   {
+  #     "role" : role_4,
+  #     "authenticate_as": role_2,
+  #     "result": "allowed",
+  #   },
+  #   {
+  #     "role" : role_1,
+  #     "authenticate_as": role_1,
+  #     "result": "disallowed",
+  #   },
+  #   {
+  #     "role" : role_2,
+  #     "authenticate_as": role_1,
+  #     "result": "disallowed",
+  #   },
+  #   {
+  #     "role" : role_1,
+  #     "authenticate_as": role_2,
+  #     "result": "disallowed",
+  #   },
+  #   {
+  #     "role" : role_2,
+  #     "authenticate_as": role_2,
+  #     "result": "disallowed",
+  #   },
+  # ]
+  #
+  # Note that, if a value has a lambda, it
+  # will be executed, giving the "test case"
+  # as parameter -- so a key-value inside it
+  # can be used to further transform the
+  # final "test case"
+  def define_contexts context_definitions = [
+      {
+      },
+    ]
+
+    key_sets = context_definitions.map(&:keys)
+    test_sets = values_product = context_definitions.map(&:values).map { |values|
+      values_in_arrays = values.map(&:to_a)
+      (["values:"].product *values_in_arrays).each(&:shift)
+    }
+
+    rv = test_sets.each_with_index.map { |test_set, context_definition_i|
+      test_set.each_with_index.map { |test, test_i|
+        test.each_with_index.map { |value, value_i|
+          key_i = value_i
+          [ key_sets[context_definition_i][key_i], value ]
+        }.to_h
+      }
+    }.flatten.map{ |test_hash| # execute lambdas, and we're done:
+      test_hash.map{ |k, v|
+        [ k, (v.call test_hash rescue v) ] # better would be rescue only NoMethodError
+      }.to_h
+    }
+
+    rv
+  end
+
 end
 
 
