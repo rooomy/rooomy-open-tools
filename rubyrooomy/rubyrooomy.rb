f08981ba58a4c2452c11df3f29d0097d682e7520
Ribamar Santarosa <ribamar-santarosa@rooomy.com>
2018-07-24 13:50:51 +0000

    open-tools: rubyrooomy.rb:   module RubyRooomyModule: improved (all the functions from RubyRooomy: added)

--- rubyrooomy/rubyrooomy.rb
+++ rubyrooomy/rubyrooomy.rb
@@ -19,6 +19,214 @@ require "rubyment"
 # needed).
 module RubyRooomyModule
 
+  # to benefit from the RubyGem functions
+  #  support and other helper functions:
+  include RubymentModule
+
+  # overrides Rubyment::rubyment_gem_defaults
+  # in order to enable a gem for RubyRooomy
+  # to be built instead.
+  def rubyment_gem_defaults args=ARGV
+    running_dir   = @memory[:running_dir]
+    basic_version = @memory[:basic_version]
+    major_version = @memory[:major_version]
+
+    gem_name,
+    gem_version,
+    gem_dir,
+    gem_ext,
+    gem_hifen,
+    gem_date,
+    gem_summary,
+    gem_description,
+    gem_authors,
+    gem_email,
+    gem_files,
+    gem_homepage,
+    gem_license,
+    gem_validate_class,
+    gem_validate_class_args,
+    gem_validate_class_method,
+    gem_is_current_file = args
+
+    gem_name ||= "rubyrooomy"
+    gem_version ||= (version [])
+    gem_dir ||= running_dir
+    gem_ext ||= ".gem"
+    gem_hifen ||= "-"
+    gem_ext ||= "date"
+    gem_date ||= "2018-07-13"
+    gem_summary     ||= "roOomy open tools for Ruby"
+    gem_description ||= "a set of ruby helpers we use mostly in the backend testing environment of roOomy"
+    gem_authors     ||= ["roOomy backend development team", "Ribamar Santarosa"]
+    gem_email       ||= 'ribamar.santarosa@rooomy.com'
+    gem_files       ||= ["lib/#{gem_name}.rb"]
+    gem_homepage    ||=
+      "http://rubygems.org/gems/#{gem_name}"
+    gem_license     ||= 'MIT'
+    gem_validate_class ||= self.class.to_s
+    gem_validate_class_args ||= {:invoke => ["puts", "rubyrooomy gem installation validated"] }
+    gem_validate_class_method ||= "new"
+    gem_is_current_file = true # this enables the possibility of building
+    #  a gem for the calling file itself, but be aware that lib/gem_file.rb
+    # is supposed to be overriden later.
+    [
+       gem_name,
+       gem_version,
+       gem_dir,
+       gem_ext,
+       gem_hifen,
+       gem_date,
+       gem_summary,
+       gem_description,
+       gem_authors,
+       gem_email,
+       gem_files,
+       gem_homepage,
+       gem_license,
+       gem_validate_class,
+       gem_validate_class_args,
+       gem_validate_class_method,
+       gem_is_current_file,
+   ]
+  end
+
+
+  # define_contexts takes as parameter
+  # an Array of Hashes having Arrays as values
+  # (which we call "basic definition of
+  # a test case") and returns an expanded
+  # Array of Hashes (which we call "an Array
+  # of test contexts"). Each Hash in the
+  # latter has the same keys of its source
+  # Hash, but instead of the Array as value, it
+  # has only one of those values. All the
+  # possible combinations will be generated.
+  #
+  # Probably it's clearer with an example:
+  # suppose in a system you want to test
+  # if a user role could create another
+  # user role.
+  #
+  # SAMPLE INPUT:
+  # [
+  #   {
+  #     "role" : [role_3, role_4],
+  #     "authenticate_as": [role_1, role_2],
+  #     "result": ["allowed"],
+  #   },
+  #   {
+  #     "role" : [role_1, role_2, role_3, role_4] - [role_3, role_4],
+  #     "authenticate_as": [role_1, role_2],
+  #     "result": ["disallowed"],
+  #   },
+  # ]
+  #
+  # SAMPLE_OUTPUT:
+  # [
+  #   {
+  #     "role" : role_3,
+  #     "authenticate_as": role_1,
+  #     "result": "allowed",
+  #   },
+  #   {
+  #     "role" : role_4,
+  #     "authenticate_as": role_1,
+  #     "result": "allowed",
+  #   },
+  #   {
+  #     "role" : role_3,
+  #     "authenticate_as": role_2,
+  #     "result": "allowed",
+  #   },
+  #   {
+  #     "role" : role_4,
+  #     "authenticate_as": role_2,
+  #     "result": "allowed",
+  #   },
+  #   {
+  #     "role" : role_1,
+  #     "authenticate_as": role_1,
+  #     "result": "disallowed",
+  #   },
+  #   {
+  #     "role" : role_2,
+  #     "authenticate_as": role_1,
+  #     "result": "disallowed",
+  #   },
+  #   {
+  #     "role" : role_1,
+  #     "authenticate_as": role_2,
+  #     "result": "disallowed",
+  #   },
+  #   {
+  #     "role" : role_2,
+  #     "authenticate_as": role_2,
+  #     "result": "disallowed",
+  #   },
+  # ]
+  #
+  # Note that, if a value has a lambda, it
+  # will be executed, giving the "test case"
+  # as parameter -- so a key-value inside it
+  # can be used to further transform the
+  # final "test case"
+  def define_contexts context_definitions = [
+      {
+      },
+    ]
+
+    key_sets = context_definitions.map(&:keys)
+    test_sets = values_product = context_definitions.map(&:values).map { |values|
+      values_in_arrays = values.map(&:to_a)
+      (["values:"].product *values_in_arrays).each(&:shift)
+    }
+
+    rv = test_sets.each_with_index.map { |test_set, context_definition_i|
+      test_set.each_with_index.map { |test, test_i|
+        test.each_with_index.map { |value, value_i|
+          key_i = value_i
+          [ key_sets[context_definition_i][key_i], value ]
+        }.to_h
+      }
+    }.flatten.map{ |test_hash| # execute lambdas, and we're done:
+      test_hash.map{ |k, v|
+        [ k, (v.call test_hash rescue v) ] # better would be rescue only NoMethodError
+      }.to_h
+    }
+
+    rv
+  end
+
+  ## SAMPLE EXAMPLES section: functions here below are provided as sample
+  # usage examples for the functions above.
+
+  # example for sample_example__define_contexts
+  # experiment with:
+  # ./rubyrooomy.rb  invoke_double puts sample_example__define_contexts
+  def sample_example__define_contexts
+    define_contexts [
+      {
+        "role" => ["role_3", "role_4"],
+        "authenticate_as"=> ["role_1", "role_2"],
+        "result"=> [
+          Proc.new { |h|
+            "#{h["authenticate_as"]} ALLOWED to create #{h["role"]}"
+          }
+        ],
+      },
+      {
+        "role" => ["role_1", "role_2", "role_3", "role_4"] - ["role_3", "role_4"],
+        "authenticate_as"=> ["role_1", "role_2"],
+        "result"=> [
+          Proc.new { |h|
+            "#{h["authenticate_as"]} NOT ALLOWED to create #{h["role"]}"
+          }
+        ],
+      },
+    ]
+  end
+
 
 end
 
